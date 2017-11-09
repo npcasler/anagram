5a1b230024c6db3912ecf1fce0e2ff0df77e9c2a
npcasler <npcasler@gmail.com>
2017-11-09 11:55:14 -0600

    Added license and corrected pep8 compliance

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -4,7 +4,7 @@
 # Utility class to extract all anagrams from a given dictionary file
 #
 # Output is a comma separated list of anagrams where the word is over 4 letters
-# long and as many or more anagrams were found than the number of letters in 
+# long and as many or more anagrams were found than the number of letters in
 # the origin words
 #
 # @param dict Path to dictionary file
@@ -14,20 +14,19 @@
 ###############################################################################
 import csv
 import pandas as pd
-import string
 
 
 class AnagramMatcher():
     dictionary = ''
-    nLetters = 0
+    count = 0
     output = ''
-    
+
     # Initialize basic dataset values
-    def __init__(self, dictionary, nLetters, output):
+    def __init__(self, dictionary, count, output):
         self.dictionary = dictionary
-        self.nLetters = nLetters
+        self.count = count
         self.output = output
-    
+
     # Read the dictionary file into memory for faster access
     def read_dict(self):
         # Create a list to hold the dictionaries
@@ -36,7 +35,7 @@ class AnagramMatcher():
             # Pre-process the dictioanry file
             for line in f:
                 # If the words has enough letters, clean and generate key
-                if len(line.strip()) >= self.nLetters:
+                if len(line.strip()) >= self.count:
                     # Remove white space and force lower-case
                     word = line.strip().lower()
                     # Sort letters alphabetically for key
@@ -53,7 +52,7 @@ class AnagramMatcher():
         # Filter out only keys with greater or equal frequency to length
         key_list = freq.loc[freq['word'] >= freq.index.str.len()]
         return key_list
-        
+
     def write_csv(self, key_list, word_list):
         # Write out data
         out_data = []
@@ -67,6 +66,3 @@ class AnagramMatcher():
             writer = csv.writer(f)
             writer.writerows(out_data)
         return 0
-
-
-
